16f97ecac2298771535b6895107ab840494bc414
ddw1234 <dineshwagh888@gmail.com>
2018-09-16 18:20:38 +0530

    listing 1-7

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -10,34 +10,13 @@ namespace CertExam
 
     public static class Program
     {
-        public static ThreadLocal<int> _field =
-        new ThreadLocal<int>(() =>
-        {
-            return Thread.CurrentThread.ManagedThreadId;
-        });
-
-
         public static void Main()
         {
-            new Thread(() =>
+            ThreadPool.QueueUserWorkItem((s) =>
             {
-                for (int x = 0; x < _field.Value; x++)
-                {
-                    Console.WriteLine("Thread A: {0}", x);
-                }
-            }).Start();
-
-
-            new Thread(() =>
-        {
-            for (int x = 0; x < _field.Value; x++)
-            {
-                Console.WriteLine("Thread B: {0}", x);
-            }
-
-        }).Start();
-
-            Console.ReadKey();
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+            Console.ReadLine();
         }
     }
 
