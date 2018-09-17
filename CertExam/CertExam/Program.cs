d5e78caa585944388fd999f4fbc1c4c0a3a3adae
ddw1234 <dineshwagh888@gmail.com>
2018-09-17 10:42:41 +0530

    Listing 1-13

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -15,22 +15,20 @@ namespace CertExam
             Task<Int32[]> parent = Task.Run(() =>
             {
                 var results = new Int32[3];
-                new Task(() => results[0] = 0,
-                TaskCreationOptions.AttachedToParent).Start();
-                new Task(() => results[1] = 1,
-                TaskCreationOptions.AttachedToParent).Start();
-                new Task(() => results[2] = 2,
-                TaskCreationOptions.AttachedToParent).Start();
+                TaskFactory tf = new TaskFactory(TaskCreationOptions.AttachedToParent,
+                TaskContinuationOptions.ExecuteSynchronously);
+                tf.StartNew(() => results[0] = 0);
+                tf.StartNew(() => results[1] = 1);
+                tf.StartNew(() => results[2] = 2);
                 return results;
             });
 
             var finalTask = parent.ContinueWith(
-            parentTask =>
-            {
-                foreach (int i in parentTask.Result)
-                    Console.WriteLine(i);
-            });
-
+                parentTask =>
+                {
+                    foreach (int i in parentTask.Result)
+                        Console.WriteLine(i);
+                });
 
             finalTask.Wait();
 
