bb662a4005de7ccb0640d6778415eef9a5b67ce9
ddw1234 <dineshwagh888@gmail.com>
2018-09-19 09:35:52 +0530

    Listing 1-25

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -13,11 +13,10 @@ namespace CertExam
     {
         public static void Main()
         {
-            var numbers = Enumerable.Range(0, 10);
+            var numbers = Enumerable.Range(0, 20);
             var parallelResult = numbers.AsParallel().AsOrdered()
-            .Where(i => i % 2 == 0)
-            .ToArray();
-            foreach (int i in parallelResult)
+            .Where(i => i % 2 == 0).AsSequential();
+            foreach (int i in parallelResult.Take(5))
                 Console.WriteLine(i);
 
             Console.ReadKey();
