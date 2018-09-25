b19421c2428380b8e05ddcd726a81dedeb4e217c
ddw1234 <dineshwagh888@gmail.com>
2018-09-26 00:09:30 +0530

    Listing 1-27

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -9,18 +9,32 @@ using System.Threading.Tasks;
 namespace CertExam
 {
 
-    public  class Program
+    public class Program
     {
         public static void Main()
         {
             var numbers = Enumerable.Range(0, 20);
-            var parallelResult = numbers.AsParallel()
-            .Where(i => i % 2 == 0);
-            parallelResult.ForAll(e => Console.WriteLine(e));
+            try
+            {
+                var parallelResult = numbers.AsParallel()
+                .Where(i => IsEven(i));
+                parallelResult.ForAll(e => Console.WriteLine(e));
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("There where { 0} exceptions", e.InnerExceptions.Count);
+            }
 
             Console.ReadKey();
         }
-     
+
+        public static bool IsEven(int i)
+        {
+            if (i % 10 == 0) throw new ArgumentException("i");
+            return i % 2 == 0;
+        }
+
+
     }
 
 
