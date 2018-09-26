cebdacba53962a12363b1ca866522984924e210d
ddw1234 <dineshwagh888@gmail.com>
2018-09-26 10:23:09 +0530

    Listing 1-31

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -15,13 +15,17 @@ namespace CertExam
         public static void Main()
         {
             ConcurrentBag<int> bag = new ConcurrentBag<int>();
-            bag.Add(42);
-            bag.Add(21);
-            int result;
-            if (bag.TryTake(out result))
-                Console.WriteLine(result);
-            if (bag.TryPeek(out result))
-                Console.WriteLine("There is a next item: {0}", result);
+            Task.Run(() =>
+            {
+                bag.Add(42);
+                Thread.Sleep(50);
+                bag.Add(21);
+            }).Wait();
+            Task.Run(() =>
+            {
+                foreach (int i in bag)
+                    Console.WriteLine(i);
+            }).Wait();
 
             Console.ReadKey();
         }
