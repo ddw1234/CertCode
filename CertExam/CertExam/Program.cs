144179b129ccbf48939cb6c3ac4f246db2d172f7
ddw1234 <dineshwagh888@gmail.com>
2018-09-30 14:27:03 +0530

    Listing 1-36

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -15,19 +15,27 @@ namespace CertExam
         public static void Main()
         {
             int n = 0;
+            object _lock = new object();
             var up = Task.Run(() =>
             {
-
-                for (int i = 0; i < 1000000; i++)
-                    n++;
+                for (int i = 0; i < 100; i++)
+                    lock (_lock)
+                        n++;
+                Console.WriteLine(n);
             });
-            
-            for (int i = 0; i < 1000000; i++)
-                n--;
+
+            Console.WriteLine( up.Id.ToString());
+            for (int i = 0; i < 100; i++)
+
+                lock (_lock)
+                    n--;
+            Console.WriteLine(n);
+
+            Console.WriteLine(up.Id.ToString());
             up.Wait();
             Console.WriteLine(n);
 
-            Console.ReadKey();
+            Console.ReadLine();
         }
 
         
