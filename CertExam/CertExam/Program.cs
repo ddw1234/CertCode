09c93feb845a4ac57f08267b6822cac68a7d654a
ddw1234 <dineshwagh888@gmail.com>
2018-10-02 19:40:57 +0530

    LISTING 1-51 Using the XOR operator

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -12,45 +12,21 @@ namespace CertExam
 
     public class Program
     {
-        private static int counter;
+       
 
         public static void Main()
         {
-            Thread T1 = new Thread(PrintStar);
-            
-            T1.Name = "One";
-            T1.Start();
-            T1.Join();
-
-
-            Thread T2 = new Thread(PrintPlus);
-
-            T2.Name = "Two";
-            T2.Start();
-            T2.Join();
-         
-
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a ^ a); // False
+            Console.WriteLine(a ^ b); // True
+            Console.WriteLine(b ^ b); // False
 
 
             Console.ReadLine();
         }
 
-        static void PrintStar()
-        {
-            for (counter = 0; counter < 5; counter++)
-            {
-                Console.Write(" * " + "\t");
-            }
-        }
-
-        private static void PrintPlus()
-        {
-            for (counter = 0; counter < 5; counter++)
-            {
-                Console.Write(" + " + "\t");
-            }
-        }
-
+     
 
     }
 
