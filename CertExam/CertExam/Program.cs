ae22c690182c01d5bf1a88e5fdceff843b6f3675
ddw1234 <dineshwagh888@gmail.com>
2018-10-02 15:37:50 +0530

    Listing 1-43

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -12,27 +12,24 @@ namespace CertExam
 
     public class Program
     {
-        static int value = 1;
+        
         public static void Main()
         {
-            CancellationTokenSource cancellationTokenSource =new CancellationTokenSource();
-
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
             CancellationToken token = cancellationTokenSource.Token;
-
             Task task = Task.Run(() =>
             {
                 while (!token.IsCancellationRequested)
                 {
                     Console.Write("*");
-                    Thread.Sleep(100);
+                    //Thread.Sleep(1000);
                 }
-            }, token);
+            }, token).ContinueWith((t) =>
+            {
+                t.Exception.Handle((e) => true);
+                Console.WriteLine("You have canceled the task");
+            }, TaskContinuationOptions.OnlyOnCanceled);
 
-
-            Console.WriteLine("Press enter to stop the task");
-            Console.ReadLine();
-            cancellationTokenSource.Cancel();
-            Console.WriteLine("Press enter to end the application");
             Console.ReadLine();
         }
 
