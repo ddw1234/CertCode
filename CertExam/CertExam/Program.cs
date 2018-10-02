bcf0e7aabe77160048571dfcbf36d63cccf61b13
ddw1234 <dineshwagh888@gmail.com>
2018-10-02 16:56:22 +0530

    implement thread-safe methods to handle race conditions

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -12,28 +12,44 @@ namespace CertExam
 
     public class Program
     {
-        
+        private static int counter;
+
         public static void Main()
         {
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-            CancellationToken token = cancellationTokenSource.Token;
-            Task task = Task.Run(() =>
-            {
-                while (!token.IsCancellationRequested)
-                {
-                    Console.Write("*");
-                    //Thread.Sleep(1000);
-                }
-            }, token).ContinueWith((t) =>
-            {
-                t.Exception.Handle((e) => true);
-                Console.WriteLine("You have canceled the task");
-            }, TaskContinuationOptions.OnlyOnCanceled);
+            Thread T1 = new Thread(PrintStar);
+            
+            T1.Name = "One";
+            T1.Start();
+            T1.Join();
+
+
+            Thread T2 = new Thread(PrintPlus);
+
+            T2.Name = "Two";
+            T2.Start();
+            T2.Join();
+         
+
+
 
             Console.ReadLine();
         }
 
-        
+        static void PrintStar()
+        {
+            for (counter = 0; counter < 5; counter++)
+            {
+                Console.Write(" * " + "\t");
+            }
+        }
+
+        private static void PrintPlus()
+        {
+            for (counter = 0; counter < 5; counter++)
+            {
+                Console.Write(" + " + "\t");
+            }
+        }
 
 
     }
