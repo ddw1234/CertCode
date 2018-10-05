3b35e61b44d13670412efd08c0f8e1190f641a78
ddw1234 <dineshwagh888@gmail.com>
2018-10-06 00:21:23 +0530

    Lisying 1-83

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -17,9 +17,14 @@ namespace CertExam
         public static void Main()
         {
             Pub p = new Pub();
-            p.OnChange += () => Console.WriteLine("Event raised to method 1");
-            p.OnChange += () => Console.WriteLine("Event raised to method 2");
+            //p.OnChange += () => Console.WriteLine("Event raised to method 1");
+            //p.OnChange += () => Console.WriteLine("Event raised to method 2");
+
+            p.OnChange += (null);
             p.Raise();
+
+            //AB a = new AB();
+            //a.CreateAndRaise();
 
 
             Console.ReadLine();
@@ -29,18 +34,39 @@ namespace CertExam
 
     }
 
-
     public class Pub
     {
-        public Action OnChange { get; set; }
+        public event Action OnChange = delegate { };
         public void Raise()
         {
-            if (OnChange != null)
-            {
-                OnChange();
-            }
+            OnChange();
         }
     }
+
+    public class AB
+    {
+        public void CreateAndRaise()
+        {
+            Pub p = new Pub();
+            p.OnChange += () => Console.WriteLine("Event raised to method 1");
+            p.OnChange += () => Console.WriteLine("Event raised to method 2");
+            p.Raise();
+        }
+    }
+
+
+    //public class Pub
+    //{
+    //    public Action OnChange { get; set; }
+    //    public void Raise()
+    //    {
+    //        //OnChange();
+    //        if (OnChange != null)
+    //        {
+    //            OnChange();
+    //        }
+    //    }
+    //}
     //public void CreateAndRaise()
     //{
     //    Pub p = new Pub();
