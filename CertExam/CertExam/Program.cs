f09561e0e4a2cb522f98f02653d12b2365cf312a
ddw1234 <dineshwagh888@gmail.com>
2018-10-07 18:49:15 +0530

    Listing 1-93

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -16,19 +16,30 @@ namespace CertExam
 
         public static void Main()
         {
-            string s = Console.ReadLine();
             try
             {
-                int i = int.Parse(s);
-                if (i == 42) Environment.FailFast("Special number entered");
+                int i = ReadAndParse();
+                Console.WriteLine("Parsed: {0}", i);
             }
-            finally
+            catch (FormatException e)
             {
-                Console.WriteLine("Program complete.");
+                Console.WriteLine("Message: {0}",e.Message);
+                Console.WriteLine("StackTrace: {0}", e.StackTrace);
+                Console.WriteLine("HelpLink: {0}", e.HelpLink);
+                Console.WriteLine("InnerException: {0}", e.InnerException);
+                Console.WriteLine("TargetSite: {0}", e.TargetSite);
+                Console.WriteLine("Source: {0}", e.Source);
             }
 
 
             Console.ReadLine();
+        }
+
+        private static int ReadAndParse()
+        {
+            string s = Console.ReadLine();
+            int i = int.Parse(s);
+            return i;
         }
 
 
