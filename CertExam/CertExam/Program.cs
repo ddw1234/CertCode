1dd0f8c33024dd73ebadd579002929ace05e7d17
ddw1234 <dineshwagh888@gmail.com>
2018-10-14 00:34:02 +0530

    Listing 2-1

--- CertExam/CertExam/Program.cs
+++ CertExam/CertExam/Program.cs
@@ -14,26 +14,28 @@ namespace CertExam
     public class Program
     {
 
-
+        [Flags]
+        enum Days
+        {
+            None = 0x0,
+            Sunday = 0x1,
+            Monday = 0x2,
+            Tuesday = 0x4,
+            Wednesday = 0x8,
+            Thursday = 0x10,
+            Friday = 0x20,
+            Saturday = 0x40
+        }
         public static void Main()
         {
-            ExceptionDispatchInfo possibleException = null;
-            try
-            {
-                string s = Console.ReadLine();
-                int.Parse(s);
-            }
-            catch (FormatException ex)
-            {
-                possibleException = ExceptionDispatchInfo.Capture(ex);
-            }
-            if (possibleException != null)
-            {
-                possibleException.Throw();
-            }
+           
+        //Days readingDays = Days.Monday | Days.Saturday | Days.Friday | Days.Sunday | Days.Tuesday | Days.Wednesday;
 
+       Days readingDays = Days.Monday | Days.Sunday;
 
-            Console.ReadLine();
+            Console.WriteLine(readingDays);
+
+        Console.ReadLine();
         }
 
         private static int ReadAndParse()
